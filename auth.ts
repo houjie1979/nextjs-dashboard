c063cacff648ebf1ccc0d36d5670e160b783b9bc
Jie Hou <hh_163com@163.com>
2024-05-09 21:40:40 +0800

    Chapter 16 Adding Metadata

--- auth.ts
+++ auth.ts
@@ -30,6 +30,7 @@ export const { auth, signIn, signOut } = NextAuth({
           const user = await getUser(email);
           if (!user) return null;
 
+          console.log("password, user.password",password, user.password)
           const passwordsMatch = await bcrypt.compare(password, user.password);
  
           if (passwordsMatch) return user;          
